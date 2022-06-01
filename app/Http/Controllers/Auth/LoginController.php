5615ca3e54db0bc32f9fba618dc674b8011bd5f8
sam778-del <samuelshina80@gmail.com>
2022-06-01 04:28:33 +0100

    Create Operator, Edit Operator, Delete Operator

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -80,6 +80,6 @@ class LoginController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect('/login');
     }
 }
